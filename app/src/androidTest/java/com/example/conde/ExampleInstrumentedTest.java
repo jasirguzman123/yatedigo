446f6d32e627fcf5b7ffbdd52f6dde951dad045c
Yasir Guzmán <koombea@0074-MarchsPro.local>
2020-04-19 19:57:06 -0500

    Change menu items color

--- app/src/androidTest/java/com/example/conde/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/conde/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.example.yatedigo", appContext.getPackageName());
+        assertEquals("com.example.conde", appContext.getPackageName());
     }
 }
